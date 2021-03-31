7cb73795d7caa8f6f04c5483cdd7d4df2b6345cf
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-03-31 16:46:27 +0800

    Add broken_score.py.py

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -22,3 +22,21 @@ def result(score):
 
 
 main()
+
+
+import random
+
+def main():
+    test = random.chocie(0-100)
+    print("Your result is:", Result(test))
+
+def Result(test):
+    if 90 <= test <= 100:
+        test = "Excellent"
+        return test
+    elif 50 <= test < 90:
+        test = "Passable"
+        return test
+    elif test < 50:
+        test = "Bad"
+        return test
