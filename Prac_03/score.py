ebbfdefb40a0c99c241a7cbf93f2dd120f0801b0
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-03-31 16:54:16 +0800

    Add broken_score.py

--- Prac_03/score.py
+++ Prac_03/score.py
@@ -1,13 +1,16 @@
-import random
-
 def main():
-    score = random.randint(0, 100)
-    print("Your score is {}".format(score))
+    score = float(input("Enter score: "))
     print("Your result is:", result(score))
 
 
 def result(score):
-    if 90 <= score <= 100:
+    if score < 0:
+        score = "Invalid score"
+        return score
+    elif score > 100:
+        score = "Invalid score"
+        return score
+    elif 90 <= score <= 100:
         score = "Excellent"
         return score
     elif 50 <= score < 90:
