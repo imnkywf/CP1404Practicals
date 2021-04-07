d3a8129d0ea3d16e592900644b1fdc9c5ac42f36
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-04-07 11:07:51 +0800

    Add lists_warmup.py

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+
+numbers = ["10", 1, 4, 1, 5, 9, 1]
+
+# Answers:
+# numbers[0] : 3
+# numbers[-1] : 2
+# numbers[3] : 1
+# numbers[:-1] : 3,1,4,1,5,9,2
+# numbers[3:4] : 1
+# 5 in numbers : True
+# 7 in numbers : False
+# "3" in numbers : False
+# numbers + [6, 5, 3] : 3,1,4,1,5,9,2,6,5,3
+
+print(numbers[2:])
+print(3 in numbers)
