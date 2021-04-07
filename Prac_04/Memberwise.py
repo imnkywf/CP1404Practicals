ce682977d99fee27efffdee93da5b53915d073a9
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-04-07 16:40:29 +0800

    add Memberwise.py

---
+++ Prac_04/Memberwise.py
@@ -0,0 +1,26 @@
+
+
+def main():
+
+    print(add_memberwise([1, 2, 3], [1, 2, 3, 4]))
+
+
+
+def add_memberwise(l1, l2):
+    a = l1[0] + l2[0]
+    b = l1[1] + l2[1]
+    c = l1[2] + l2[2]
+    d = 0 + l2[3]
+
+    return a, b, c, d
+
+
+
+
+
+
+
+
+
+
+main()
