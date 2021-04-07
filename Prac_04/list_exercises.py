4174d95688d9d51a0609a15a374fe1e6bfc6793c
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-04-07 14:08:41 +0800

    Add list_exercises.py

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -1,7 +1,27 @@
 def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+
     numbers = []
-    for numbers in range(5):
+    name = str(input("What is your name: "))
+    if name in usernames:
+        print("Access granted")
+    else:
+        print("Access denied.")
+
+    for number in range(5):
         number = int(input("Number: "))
+        numbers.append(number)
+
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers)/5))
+
+
+
 
 
 
