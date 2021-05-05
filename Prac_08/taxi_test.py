4a070a369452c52a44d3f7e31fb379b05c39f6d2
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-05-05 11:41:19 +0800

    changed taxi.py and taxi_test.py

--- Prac_08/taxi_test.py
+++ Prac_08/taxi_test.py
@@ -1,9 +1,13 @@
 from taxi import Taxi
 
-def main():
 
-    taxi = Taxi("Prius 1", 100, 1.23)
+def main():
+    taxi = Taxi("Prius 1", 100)
     taxi.drive(40)
+    print(taxi)
+
+    taxi.start_fare()
+    taxi.drive(100)
     print(taxi)
 
 
