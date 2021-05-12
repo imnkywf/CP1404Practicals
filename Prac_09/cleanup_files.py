3ea2930038ea6dd343e439924fbd5cf094712c6e
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-05-12 13:21:49 +0800

    finished cleanup_files.py

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -6,32 +6,6 @@ import shutil
 import os
 
 
-def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
-
-    # Change to desired directory
-    os.chdir('Lyrics')
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
 
 
 
@@ -42,7 +16,20 @@ def get_fixed_filename(filename):
     return new_name
 
 
+def main():
+    """Process all subdirectories using os.walk()."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
 
+        for filename in filenames:
+            name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(name, new_name)
+            print("Renaming {} to {}".format(filename, new_name))
 
 
 main()
