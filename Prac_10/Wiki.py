6f490adc4f921c496229dd063d8899540355a177
imnkywf <yiwei.fan@my.jcu.edu.au>
2021-05-19 13:48:29 +0800

    finish prac 10

--- Prac_10/Wiki.py
+++ Prac_10/Wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
 
-search_phrase = input("Enter a page title or search phrase: ")
+search_phrase = input("Enter a page title or search phrase : ")
 while search_phrase != "":
     try:
         page = wikipedia.page(search_phrase)
